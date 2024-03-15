e204858c61b67d948f97af095ae5ca6a662875d8
nanaisisi <131863615+nanaisisi@users.noreply.github.com>
2024-03-15 17:05:16 +0900

    1.0.9

--- script/companion.js
+++ script/companion.js
@@ -1,9 +1,9 @@
 const companion_pre_bool = localStorage.getItem("companion_switch_local");
 let companion_switch_display = 'none';
-switch (companion_pre_bool){
-    case "0":
-    case "1":
-        companion_switch_display = "block"
+switch (companion_pre_bool) {
+  case "0":
+  case "1":
+    companion_switch_display = "block"
 }
 
 const companion = document.getElementById('companion')
