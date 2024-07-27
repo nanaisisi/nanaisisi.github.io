fef3ed40f9177014ba9aabd64cd3a940a4912008
nanaisisi <131863615+nanaisisi@users.noreply.github.com>
2024-07-27 20:48:00 +0900

    ok

--- script/companion.js
+++ script/companion.js
@@ -1,12 +1,30 @@
-const companion_pre_bool = localStorage.getItem("companion_switch_local");
-let companion_switch_display = 'none';
-switch (companion_pre_bool) {
-  case "0":
-  case "1":
-    companion_switch_display = "block"
-}
+function tp_top() {
+	const tp_top_btn = document.getElementById("tp_top_btn");
+	/** @type {HTMLFrameElement} */
+	const top_iframe = document.getElementById("top_iframe");
+	if (top_iframe) {
+		const top_iframe_window = top_iframe.contentWindow;
+		if (tp_top_btn) {
+			tp_top_btn.addEventListener("click", () => {
+				if (top_iframe_window)
+					top_iframe_window.scroll({
+						top: 0,
+						behavior: "smooth",
+					});
+			});
+		}
+	}
 
-const companion = document.getElementById('companion')
-if (companion) {
-  companion.style.display = companion_switch_display
+	const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+	const darkModeOn = darkModeMediaQuery.matches;
+	const color_scheme_btn = document.getElementById("scheme_toggle");
+	if (color_scheme_btn) {
+		color_scheme_btn.addEventListener("click", () => {
+			if (darkModeOff) {
+				document.documentElement.classList.add("darkmode");
+			} else {
+				document.documentElement.classList.remove("darkmode");
+			}
+		});
+	}
 }
