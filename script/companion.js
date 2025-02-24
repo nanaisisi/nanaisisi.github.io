746fe319914ea600ba45bc9930cc4f69185a90b5
nanaisisi <131863615+nanaisisi@users.noreply.github.com>
2025-02-24 19:45:05 +0900

    ok

--- script/companion.js
+++ script/companion.js
@@ -28,6 +28,7 @@ function tp_top() {
 		});
 	}
 }
+
 function menu_button() {
 	const number_display = document.getElementById("number_display");
 	const count_up_btn = document.getElementById("count_up_btn");
@@ -54,3 +55,43 @@ function menu_button() {
 		}
 	};
 }
+
+// 旧暦の和風月名を表示する関数
+function displayJapaneseMonthName() {
+	const japaneseMonthNames = [
+		"睦月",
+		"如月",
+		"弥生",
+		"卯月",
+		"皐月",
+		"水無月",
+		"文月",
+		"葉月",
+		"長月",
+		"神無月",
+		"霜月",
+		"師走",
+	];
+	const currentMonth = new Date().getMonth(); // 0から11の値を返す
+	const japaneseMonthName = japaneseMonthNames[currentMonth];
+	const monthNameElement = document.getElementById("japanese_month_name");
+	if (monthNameElement) {
+		monthNameElement.textContent = japaneseMonthName;
+	}
+}
+
+// ページロード
+window.addEventListener("load", () => {
+	if (tp_top()) {
+		tp_top();
+	}
+});
+
+window.addEventListener("load", () => {
+	if (menu_button()) {
+		menu_button();
+	}
+});
+window.addEventListener("load", () => {
+	displayJapaneseMonthName();
+});
