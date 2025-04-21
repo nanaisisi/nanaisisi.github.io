c23af97644455bc4d21028a683d0853f3688971c
nanaisisi <masakuni_gm@yahoo.co.jp>
2025-04-21 18:20:36 +0900

    ok

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
-import { serve } from "https://deno.land/std@0.207.0/http/server.ts";
+// @deno-types="https://deno.land/std@0.224.0/http/server.ts"
+import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
 
 // MIMEタイプのマッピング
 const mimeTypes = {
