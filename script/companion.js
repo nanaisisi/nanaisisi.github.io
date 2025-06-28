936a0678f65f7585dd1317498604015a78f4427c
nanaisisi <131863615+nanaisisi@users.noreply.github.com>
2025-06-29 04:17:13 +0900

    ok perhaps

--- script/companion.js
+++ script/companion.js
@@ -1,7 +1,7 @@
 // サイト全体の機能を統合するメインスクリプト
 import { initMenuButton } from "./menu-handler.js";
-import { initThemeManager } from "./theme-manager.js";
 import { initMonthDisplay } from "./month-display.js";
+import { initThemeManager } from "./theme-manager.js";
 
 // ページのロード時に各機能を初期化
 window.addEventListener("load", async () => {
