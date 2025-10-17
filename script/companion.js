5a2c1b6d441fb2c1c78ded90d744d889665b7ed3
nanaisisi <131863615+nanaisisi@users.noreply.github.com>
2025-10-17 23:13:08 +0900

    file rename

--- script/companion.js
+++ script/companion.js
@@ -1,7 +1,7 @@
 // サイト全体の機能を統合するメインスクリプト
-import { initMenuButton } from "./menu-handler.js";
-import { initMonthDisplay } from "./month-display.js";
-import { initThemeManager } from "./theme-manager.js";
+import { initMenuButton } from "./menu_handler.js";
+import { initMonthDisplay } from "./month_display.js";
+import { initThemeManager } from "./theme_manager.js";
 
 // ページのロード時に各機能を初期化
 window.addEventListener("load", async () => {
